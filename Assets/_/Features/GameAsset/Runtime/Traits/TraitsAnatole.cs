cc802afd31c2446b1ec6c5a8068f5b6fc09ca04f
Anatole Wattiez <a.wattiez95@gmail.com>
2023-05-23 17:29:56 +0200

    [Traits] Created C# Object
    For testing purposes

---
+++ Assets/_/Features/GameAsset/Runtime/Traits/TraitsAnatole.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class TraitsAnatole
+    {
+    	#region Public Members
+
+
+
+    	#endregion
+
+
+    	#region Unity API
+
+
+
+    	#endregion
+
+
+    	#region Main Methods
+
+
+
+    	#endregion
+
+
+    	#region Private and Protected
+
+
+
+    	#endregion
+    }
+}
