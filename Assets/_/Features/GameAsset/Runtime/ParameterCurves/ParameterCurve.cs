7aef3bff925238047030d897dc9947dc15ed1def
Joseph Prygiel <PrygielJoseph@gmail.com>
2023-05-23 17:10:32 +0200

    [ParameterCurves] : Script add base

---
+++ Assets/_/Features/GameAsset/Runtime/ParameterCurves/ParameterCurve.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class ParameterCurve 
+    {
+
+        #region Public Members
+
+        public int[] m_hpMaxValue;
+        public int[] m_mpMaxValue;
+        public int[] m_attackValue;
+        public int[] m_defenseValue;
+        public int[] m_attackMValue;
+        public int[] m_defenseMValue;
+        public int[] m_agilityValue;
+        public int[] m_luckValue;
+
+
+        #endregion
+
+        #region Unity API
+
+        #endregion
+        #region Main Methods
+        #endregion
+
+        #region Utils
+        #endregion
+
+        #region Private and Protected Members
+        #endregion
+
+
+    }
+}
