69c0a7eb6b4f854ed9d7098d5af2d9eabfa4ed9c
Thomas_Dupont <thom.dev30@gmail.com>
2023-05-23 14:07:34 +0200

    [Class]: ClassData modified and add Variable

--- Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
+++ Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
@@ -4,43 +4,37 @@ using UnityEngine;
 
 namespace GameAsset.Runtime
 {
-    public class ClassData : MonoBehaviour
+    [CreateAssetMenu]
+    public class ClassData : ScriptableObject
     {
-    	#region Public Member
-	
-    	#endregion
-	
-	
-    	#region Unity API
-	
-    	void Awake()
-    	{
-		
-    	}
-	
-        void Start()
-        {
-        
-        }
 
-        void Update()
+        #region Public Member
+
+            public int m_maxChange;
+            public string m_name;
+            public int m_baseValue;
+            public int m_supValue;
+            public int m_acceleratorA;
+            public int m_acceleratorB;
+            public int[] m_XP;
+            public int[] m_TotalXP;
+            //l'index du tableau sert pour le niveau
+            public int[] m_hpMaxValue;
+            public int[] m_mpMaxValue;
+            public int[] m_attackValue;
+            public int[] m_defenseValue;
+            public int[] m_attackMValue;
+            public int[] m_defenseMValue;
+            public int[] m_agilityValue;
+            public int[] m_luckValue;
+        //public AnimationCurve m_hpMaxCurve;
+
+        #endregion
+
+
+        public void OnDisable()
         {
-        
+            
         }
-	
-    	void FixedUpdate()
-    	{
-		
-    	}
-	
-    	#endregion
-	
-    	#region Main Methods
-	
-    	#endregion
-	
-    	#region Private And Protect
-	
-    	#endregion
     }
 }
