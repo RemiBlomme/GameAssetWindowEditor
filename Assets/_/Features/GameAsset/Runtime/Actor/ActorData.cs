a1f3c8a839a62d632b83320bd541905e61cbff8c
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-23 15:32:59 +0200

    [Actor] Added Actor scriptableObject

--- Assets/_/Features/GameAsset/Runtime/Actor/ActorData.cs
+++ Assets/_/Features/GameAsset/Runtime/Actor/ActorData.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 namespace GameAsset.Runtime
 {
-    [CreateAssetMenu(menuName = "Game Database/Actor")]
+    [CreateAssetMenu(fileName = "New actor", menuName = "Game Database/Actor")]
     public class ActorData : ScriptableObject
     {
         #region Public Members
 
+        [Header("General Settings")]
+        public string m_name;
+        public string m_surname;
+        //public ClassData m_class; // TODO change variable type
+        public int m_initialLevel;
+        public int m_maxLevel;
+        public string m_profile;
+        [Space]
+        [Header("Images")]
+        public Sprite m_faceSprite;
+        public Sprite[] m_characterSprites;
+        public Sprite m_fightingSprite;
+        //[Header("Initial Equipment")]
+        // public WeaponData m_weapon; // TODO change variable type
+        // public ShieldData m_shield; // TODO change variable type
+        // public HeadData m_head; // TODO change variable type
+        // public BodyData m_bodies; // TODO change variable type
+        // public AccessoryData m_accessories; // TODO change variable type
 
 
         #endregion
@@ -21,7 +39,7 @@ namespace GameAsset.Runtime
 
         #region Main Methods
 
-
+        
 
         #endregion
 
@@ -35,27 +53,6 @@ namespace GameAsset.Runtime
 
         #region Private and Protected Members
 
-        [Header("Properties")]
-        [SerializeField] private string _name;
-        [SerializeField] private string _surname;
-        [SerializeField] private string _profile;
-        [Space]
-        //[SerializeField] private ClassData _class; // TODO change variable type
-        [Space]
-        [SerializeField] private int _initialLevel;
-        [SerializeField] private int _maxLevel;
-        [Space]
-        [Header("Initial Equipments")]
-        //[SerializeField] private WeaponData _weapon; // TODO change variable type
-        //[SerializeField] private ShieldData _shield; // TODO change variable type
-        //[SerializeField] private EquipmentData _head; // TODO change variable type
-        //[SerializeField] private EquipmentData _bodies; // TODO change variable type
-        //[SerializeField] private EquipmentData _accessories; // TODO change variable type
-        [Space]
-        [Header("Sprites")]
-        [SerializeField] private Sprite _faceSprite;
-        [SerializeField] private Sprite[] _characterSprites;
-        [SerializeField] private Sprite _fightingSprite;
 
         #endregion
     }
