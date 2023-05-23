d8388fd50256cfe0a2b2571241f9d15150a4a0ef
Gabor Vilics <vilics@hotmail.com>
2023-05-23 17:29:30 +0200

    [Traits]: Created C# Object

---
+++ Assets/_/Features/GameAsset/Runtime/Traits/TraitsGabor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class TraitsGabor
+    {
+        public ArmorTypeData[] _armorTypes;
+        public ElementTypeData[] _elementTypes;
+        public EquipmentTypeData[] _equipmentTypes;
+        public SkillTypeData[] _skillTypes;
+        public WeaponTypeData[] _weaponTypes;
+        
+       public enum States
+        {
+            Assomé,
+
+        }
+    }
+}
