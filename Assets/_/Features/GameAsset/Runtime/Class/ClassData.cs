79b23704db7da36936a6baadeac30da29ebf2ef3
Thomas_Dupont <thom.dev30@gmail.com>
2023-05-23 14:17:40 +0200

    [Class]: ClassData: Correction indentation

--- Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
+++ Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
@@ -7,26 +7,24 @@ namespace GameAsset.Runtime
     [CreateAssetMenu]
     public class ClassData : ScriptableObject
     {
-
         #region Public Member
-
-            public int m_maxChange;
-            public string m_name;
-            public int m_baseValue;
-            public int m_supValue;
-            public int m_acceleratorA;
-            public int m_acceleratorB;
-            public int[] m_XP;
-            public int[] m_TotalXP;
-            //l'index du tableau sert pour le niveau
-            public int[] m_hpMaxValue;
-            public int[] m_mpMaxValue;
-            public int[] m_attackValue;
-            public int[] m_defenseValue;
-            public int[] m_attackMValue;
-            public int[] m_defenseMValue;
-            public int[] m_agilityValue;
-            public int[] m_luckValue;
+        public int m_maxChange;
+        public string m_name;
+        public int m_baseValue;
+        public int m_supValue;
+        public int m_acceleratorA;
+        public int m_acceleratorB;
+        public int[] m_XP;
+        public int[] m_TotalXP;
+        //l'index du tableau sert pour le niveau
+        public int[] m_hpMaxValue;
+        public int[] m_mpMaxValue;
+        public int[] m_attackValue;
+        public int[] m_defenseValue;
+        public int[] m_attackMValue;
+        public int[] m_defenseMValue;
+        public int[] m_agilityValue;
+        public int[] m_luckValue;
         //public AnimationCurve m_hpMaxCurve;
 
         #endregion
