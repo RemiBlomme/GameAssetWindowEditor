7ef217c6b96b4c4fcede88b022d488c2e470dbf9
Thomas_Dupont <thom.dev30@gmail.com>
2023-05-23 13:53:58 +0200

    [Class]: Create Folder

---
+++ Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class ClassData : MonoBehaviour
+    {
+    	#region Public Member
+	
+    	#endregion
+	
+	
+    	#region Unity API
+	
+    	void Awake()
+    	{
+		
+    	}
+	
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+	
+    	void FixedUpdate()
+    	{
+		
+    	}
+	
+    	#endregion
+	
+    	#region Main Methods
+	
+    	#endregion
+	
+    	#region Private And Protect
+	
+    	#endregion
+    }
+}
