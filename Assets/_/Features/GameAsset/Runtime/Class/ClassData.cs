2ee5a9aa80595f895a5b62d67522ebf5a509f233
Joseph Prygiel <PrygielJoseph@gmail.com>
2023-05-23 15:21:40 +0200

    [Class] : ClassData public values in inspector

--- Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
+++ Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
@@ -7,13 +7,22 @@ namespace GameAsset.Runtime
     [CreateAssetMenu]
     public class ClassData : ScriptableObject
     {
+        
+
         #region Public Member
-        public int m_maxChange;
-        public string m_name;
-        public int m_baseValue;
-        public int m_supValue;
-        public int m_acceleratorA;
-        public int m_acceleratorB;
+        //-------------- |
+        //Fenetre Classes (A mettre dans un autre script, la selection ne dois pas figurer dans chaque classe)
+        //public string m_nameTitle;
+        //public int m_maxChange;
+        //-------------- |
+
+        //Fenetre Parametres généraux 
+        public string m_className;
+        public int m_xpCourbBaseValue = 30;
+        public int m_xpCourbSupValue = 20;
+        public int m_xpCourbAcceleratorA = 30;
+        public int m_xpCourbAcceleratorB = 30;
+
         public int[] m_XP;
         public int[] m_TotalXP;
         //l'index du tableau sert pour le niveau
@@ -30,9 +39,23 @@ namespace GameAsset.Runtime
         #endregion
 
 
-        public void OnDisable()
-        {
+        #region Unity API
+           public void OnDisable()
+                {
             
-        }
+                }
+        #endregion
+
+        #region Main Methods
+
+
+
+        #endregion
+
+        #region Utils
+        #endregion
+
+        #region Private and Protected Members
+        #endregion
     }
 }
---
+++ Assets/_/Features/GameAsset/Runtime/Class/TestData.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class TestData : MonoBehaviour
+    {
+
+    #region Public Members
+    #endregion
+
+    #region Unity API
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    #endregion
+    #region Main Methods
+    #endregion
+
+    #region Utils
+    #endregion
+
+    #region Private and Protected Members
+    #endregion
+
+
+    }
+}
