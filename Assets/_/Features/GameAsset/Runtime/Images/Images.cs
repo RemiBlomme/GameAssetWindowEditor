6d78983df13c274982c50665d00d5ad6d26d3c61
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-24 14:41:43 +0200

    [Images] Add Images script

---
+++ Assets/_/Features/GameAsset/Runtime/Images/Images.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    [Serializable]
+    public class Images : MonoBehaviour
+    {
+        #region Public Members
+
+        public Sprite m_faceSprite;
+        public Sprite m_characterSprite;
+        public Sprite m_fighterSprite;
+
+    	#endregion
+    }
+}
