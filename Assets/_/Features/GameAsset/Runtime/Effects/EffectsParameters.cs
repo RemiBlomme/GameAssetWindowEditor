2caac1725f36bd3114f942ed77c5d606df41c93b
Cyril Delavallée <cyril.delavallee@hotmail.com>
2023-05-24 13:53:47 +0200

    [Effects] wip effects parameters

---
+++ Assets/_/Features/GameAsset/Runtime/Effects/EffectsParameters.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class EffectsParameters
+    {
+        public enum EffectsParametersActions
+        {
+            AddBuff,
+            AddDenuff,
+            RemoveBuff,
+            RemoveDebuff,
+        }
+    }
+}
