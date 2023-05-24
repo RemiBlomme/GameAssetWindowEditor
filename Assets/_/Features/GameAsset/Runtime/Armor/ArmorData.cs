67b0ea72aa4394ffbca97b3699bbeca165930d96
Gabor Vilics <vilics@hotmail.com>
2023-05-24 17:26:20 +0200

    [Armor]: Added Line in ArmorData

--- Assets/_/Features/GameAsset/Runtime/Armor/ArmorData.cs
+++ Assets/_/Features/GameAsset/Runtime/Armor/ArmorData.cs
@@ -5,43 +5,8 @@ namespace GameAsset.Runtime
     [CreateAssetMenu]
     public class ArmorData : ScriptableObject   
     {
-        //public enum ArmorTypes
-        //{
-        //    None,
-        //    General,
-        //    Magic,
-        //    Light,
-        //    Heavy,
-        //    SmallShield,
-        //    BigShield
-        //}
-        //
-        //public enum EquipmentTypes
-        //{
-        //    Shield,
-        //    Head,
-        //    Chest,
-        //    Legs
-        //}
-        //
-        //public string m_name;
-        //public Texture m_icon;
-        //public string m_description;
-        //
-        //public ArmorTypes m_armorType;
-        //public EquipmentTypes m_equipmentType;
-        //public int m_price;
-        //
-        //public int m_attack;
-        //public int m_agility;
-        //public int m_defense;
-        //public int m_luck;
-        //public int m_attackM;
-        //public int m_HPMax;
-        //public int m_DefenseM;
-        //public int m_MPMax;
-
         public ParameterChange m_parameterChange;
         public Traits m_traits;
+        public GeneralParametersArmor m_generalParametersArmor;
     }
 }
