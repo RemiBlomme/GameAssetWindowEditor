b6c8c54fd004801a148a6d73826b9173d1ab2c18
Gattesco_Dorian <doriangattesco@gmail.com>
2023-05-24 09:59:12 +0200

    [ParameterChange] Clean Script

--- Assets/_/Features/GameAsset/Runtime/ParameterChange/ParameterChange.cs
+++ Assets/_/Features/GameAsset/Runtime/ParameterChange/ParameterChange.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace GameAsset.Runtime
 {
@@ -13,21 +10,5 @@ namespace GameAsset.Runtime
         public int m_damage, m_defense, m_magicDamage, m_magicDefense, m_agility, m_luck, m_maxHP, m_maxMP;
 
         #endregion Public Members
-
-        #region Unity API
-
-        #endregion Unity API
-
-        #region Main Methods
-
-        #endregion Main Methods
-
-        #region Utils
-
-        #endregion Utils
-
-        #region Private and Protected Members
-
-        #endregion Private and Protected Members
     }
 }
