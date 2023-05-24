ba2b92d406b157f9c762652f84d8d8481f21ec1a
Gattesco_Dorian <doriangattesco@gmail.com>
2023-05-24 11:24:20 +0200

    [InitialEquipement] Add Script

---
+++ Assets/_/Features/GameAsset/Runtime/InitialEquipement/InitialEquipement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Runtime
+{
+    public class InitialEquipement
+    {
+        #region Public Members
+
+        #endregion
+
+        #region Unity API
+
+        #endregion
+
+        #region Main Methods
+
+        #endregion
+
+        #region Utils
+
+        #endregion
+
+        #region Private and Protected Members
+
+        #endregion
+    }
+}
