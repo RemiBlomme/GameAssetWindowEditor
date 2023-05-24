c59c6424007c6865570af38a50d7cd4941704b10
Joseph Prygiel <PrygielJoseph@gmail.com>
2023-05-24 11:48:51 +0200

    [ParameterCurves] : Variables names minMax add

--- Assets/_/Features/GameAsset/Runtime/ParameterCurves/ParameterCurves.cs
+++ Assets/_/Features/GameAsset/Runtime/ParameterCurves/ParameterCurves.cs
@@ -9,29 +9,46 @@ namespace GameAsset.Runtime
 
         #region Public Members
 
+        public AnimationCurve m_hpCurve;
         public int m_hpMaxLvl;
+        public int m_hpMinLvlValue;
         public int m_hpMaxLvlValue;
 
+        public AnimationCurve m_mpCurve;
         public int m_mpMaxLvl;
+        public int m_mpMinLvlValue;
         public int m_mpMaxLvlValue;
 
+        public AnimationCurve m_attackCurve;
         public int m_attackLvl;
-        public int m_attackLvlValue;
+        public int m_attackMinLvlValue;
+        public int m_attackMaxLvlValue;
 
+        public AnimationCurve m_defenseCurve;
         public int m_defenseLvl;
-        public int m_defenseLvlValue;
+        public int m_defenseMinLvlValue;
+        public int m_defenseMaxLvlValue;
 
+        public AnimationCurve m_attackMCurve;
         public int m_attackMLvl;
-        public int m_attackMValue;
+        public int m_attackMMinValue;
+        public int m_attackMMaxValue;
 
+        public AnimationCurve m_defenseMCurve;
         public int m_defenseMLvl;
-        public int m_defenseMLvlValue;
+        public int m_defenseMMinLvlValue;
+        public int m_defenseMMaxLvlValue;
 
+        public AnimationCurve m_agilityCurve;
         public int m_agilityLvl;
-        public int m_agilityLvlValue;
+        public int m_agilityMinLvlValue;
+        public int m_agilityMaxLvlValue;
 
+        public AnimationCurve m_luckCurve;
         public int m_luckLvl;
-        public int m_luckLvlValue;
+        public int m_luckMinLvlValue;
+        public int m_luckMaxLvlValue;
+       
 
         #endregion
 
