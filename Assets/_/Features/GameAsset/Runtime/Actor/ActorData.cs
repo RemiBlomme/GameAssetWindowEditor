557215c3668aa2637463c17291d75abd219b5533
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-25 11:36:53 +0200

    [Actor] Add sprite for Actor, Update ActorData script with the references of the right features

--- Assets/_/Features/GameAsset/Runtime/Actor/ActorData.cs
+++ Assets/_/Features/GameAsset/Runtime/Actor/ActorData.cs
@@ -2,57 +2,16 @@ using UnityEngine;
 
 namespace GameAsset.Runtime
 {
-    [CreateAssetMenu(fileName = "New actor", menuName = "Game Database/Actor")]
+    [CreateAssetMenu(fileName = "ActorData", menuName = "Game Database/Actor")]
     public class ActorData : ScriptableObject
     {
         #region Public Members
 
-        [Header("General Settings")]
-        public string m_name;
-        public string m_surname;
-        //public ClassData m_class; // TODO change variable type
-        public int m_initialLevel;
-        public int m_maxLevel;
-        public string m_profile;
-        [Space]
-        [Header("Images")]
-        public Sprite m_faceSprite;
-        public Sprite[] m_characterSprites;
-        public Sprite m_fightingSprite;
-        //[Header("Initial Equipment")]
-        //public WeaponData m_weapon; // TODO change variable type
-        //public ShieldData m_shield; // TODO change variable type
-        //public HeadData m_head; // TODO change variable type
-        //public BodyData m_bodies; // TODO change variable type
-        //public AccessoryData m_accessories; // TODO change variable type
-
-
-        #endregion
-
-
-        #region Unity API
-
-
-
-        #endregion
-
-
-        #region Main Methods
-
-        
-
-        #endregion
-
-
-        #region Utils
-
-
-
-        #endregion
-
-
-        #region Private and Protected Members
-
+        public GeneralParametersActor m_generalParameters;
+        public Images m_images;
+        public InitialEquipement m_initialEquipement;
+        public Traits m_traits;
+        public Comment m_comment;
 
         #endregion
     }
