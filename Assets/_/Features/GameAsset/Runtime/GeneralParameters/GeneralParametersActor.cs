3b756e8a81e915d06e9b5ec41e2bf8eb24ded5c6
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-25 12:26:49 +0200

    [GeneralParameters] Use the right variable in the GeneralParameters script

--- Assets/_/Features/GameAsset/Runtime/GeneralParameters/GeneralParametersActor.cs
+++ Assets/_/Features/GameAsset/Runtime/GeneralParameters/GeneralParametersActor.cs
@@ -9,7 +9,7 @@ namespace GameAsset.Runtime
 
         public string m_username;
         public string m_profile;
-        public string m_class; // TODO Change variable type
+        public ClassData m_class; 
         public int m_initialLevel;
         public int m_maxLevel;
 
