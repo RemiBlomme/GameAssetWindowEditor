3dbe73f650a857c16ca6c0b58c64576d8ec5aace
Gabor Vilics <vilics@hotmail.com>
2023-05-25 16:00:25 +0200

    [Traits]: set traits gui script

---
+++ Assets/_/Features/GameAsset/Editor/Traits/TraitsGUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAsset.Editor
+{
+    public class TraitsGUI : UnityEditor.EditorWindow
+    {
+        #region Public Members
+        #endregion
+
+
+        #region Unity API
+        private void OnGUI()
+        {
+            DisplayTraitsWindow();
+        }
+        #endregion
+
+
+        #region Main Methods
+
+        public void DisplayTraitsWindow()
+        {
+            //switch with differentTabs
+        }
+
+        #endregion
+    }
+}
