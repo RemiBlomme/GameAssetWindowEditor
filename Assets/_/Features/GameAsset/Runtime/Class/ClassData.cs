9e3d07a1edc813d19e378cb8259880e25bb1e955
Thomas_Dupont <thom.dev30@gmail.com>
2023-05-25 10:06:34 +0200

    [Class]: delete: ClassData, ClassTest. Rename Class: ClassData

--- Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
+++ Assets/_/Features/GameAsset/Runtime/Class/ClassData.cs
@@ -1,54 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameAsset.Runtime
 {
-    [CreateAssetMenu]
+    [CreateAssetMenu(fileName="ClassData", menuName ="Game Database/Class")]
     public class ClassData : ScriptableObject
     {
-        
-
         #region Public Member
-        //-------------- |
-        //Fenetre Classes (A mettre dans un autre script, la selection ne dois pas figurer dans chaque classe)
-        //public string m_nameTitle;
-        //public int m_maxChange;
-        //-------------- |
 
-        //Fenetre Parametres généraux 
-        public string m_className;
-        public int m_xpCourbBaseValue = 30;
-        public int m_xpCourbSupValue = 20;
-        public int m_xpCourbAcceleratorA = 30;
-        public int m_xpCourbAcceleratorB = 30;
-
-        public int[] m_XP;
-        public int[] m_TotalXP;
-        //l'index du tableau sert pour le niveau
-  
-        //public AnimationCurve m_hpMaxCurve;
-
-        #endregion
-
-
-        #region Unity API
-           public void OnDisable()
-                {
-            
-                }
-        #endregion
-
-        #region Main Methods
-
-
-
-        #endregion
-
-        #region Utils
-        #endregion
-
-        #region Private and Protected Members
-        #endregion
+        public GeneralParametersClass m_generalParametersClass;
+        public ParameterCurve m_parameterCurve;
+        public SkillsToLearn m_skillsToLearn;
+        public Traits m_traits;
+        public Comment m_comment;
+	
+    	#endregion
     }
 }
