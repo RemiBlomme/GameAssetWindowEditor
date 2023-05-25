57078a874657d12e0b62ec1e7a18dd647f2f597b
Joseph Prygiel <PrygielJoseph@gmail.com>
2023-05-25 16:22:36 +0200

    [ParameterCurves] : Creating all scriptsGUI EditorWindow

---
+++ Assets/_/Features/GameAsset/Editor/ParameterCurves/CurveMagicDefenseGUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace GameAsset.Editor
+{
+    public class CurveMagicDefenseGUI : EditorWindow
+    {
+
+    #region Public Members
+    #endregion
+
+    #region Unity API
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    #endregion
+    #region Main Methods
+    #endregion
+
+    #region Utils
+    #endregion
+
+    #region Private and Protected Members
+    #endregion
+
+
+    }
+}
---
+++ Assets/_/Features/GameAsset/Editor/ParameterCurves/ParameterCurvesGUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameAsset.Editor
+{
+    public class ParameterCurvesGUI : EditorWindow
+    {
+
+    #region Public Members
+    #endregion
+
+    #region Unity API
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    #endregion
+    #region Main Methods
+    #endregion
+
+    #region Utils
+    #endregion
+
+    #region Private and Protected Members
+    #endregion
+
+
+    }
+}
