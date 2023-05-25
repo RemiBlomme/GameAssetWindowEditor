7d69d793b165db0da69f7b25e3373178ad2d9878
Gattesco Dorian <doriangattesco@gmail.com>
2023-05-25 12:07:23 +0200

    [Weapon] Add Icon Sprite Sword

--- Assets/_/Features/GameAsset/Runtime/Weapon/WeaponData.cs
+++ Assets/_/Features/GameAsset/Runtime/Weapon/WeaponData.cs
@@ -1,34 +1,17 @@
-using GameAsset.Runtime;
 using UnityEngine;
 
 namespace GameAsset.Runtime
 {
-    [CreateAssetMenu(fileName = "Weapon", menuName = "Game Database/Weapon")]
+    [CreateAssetMenu(fileName = "WeaponData", menuName = "Game Database/Weapon")]
     public class WeaponData : ScriptableObject
     {
         #region Public Members
 
         public GeneralParametersWeapon m_generalParametersWeapon;
-        public ParameterChange m_change;
+        public ParameterChange m_parameterChange;
         public Traits m_traits;
         public Comment m_comment;
 
-        #endregion Public Members
-
-        #region Unity API
-
-        #endregion Unity API
-
-        #region Main Methods
-
-        #endregion Main Methods
-
-        #region Utils
-
-        #endregion Utils
-
-        #region Private and Protected Members
-
-        #endregion Private and Protected Members
+        #endregion
     }
 }
