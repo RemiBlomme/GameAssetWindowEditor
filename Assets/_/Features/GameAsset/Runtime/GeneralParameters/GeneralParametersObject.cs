7c9bde475d38c1bbc4fc2df5dfd1f4b3d23f12e4
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-25 12:23:09 +0200

    [GeneralParameters] Update variable in the GeneralParametersObject script

--- Assets/_/Features/GameAsset/Runtime/GeneralParameters/GeneralParametersObject.cs
+++ Assets/_/Features/GameAsset/Runtime/GeneralParameters/GeneralParametersObject.cs
@@ -10,7 +10,7 @@ namespace GameAsset.Runtime
 
         public Sprite m_icon;
         public string m_description;
-        public string m_objectType; // TODO Change variable type
+        public ObjectData m_objectType; 
         public int m_price;
         public bool m_isEdible;
         public ApplicationField m_applicationField;
