7342fb18ddd5aa13ce26ab562ae2ed8321088c45
Cyril Delavallée <cyril.delavallee@hotmail.com>
2023-05-25 11:02:05 +0200

    [Effects] refactor and rename scripts

--- Assets/_/Features/GameAsset/Runtime/Effects/EffectsOther.cs
+++ Assets/_/Features/GameAsset/Runtime/Effects/EffectsOther.cs
@@ -1,41 +1,32 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace GameAsset.Runtime
 {
     [Serializable]
-    public class OtherEffects : EffectComponents
+    public class EffectsOther : EffectComponents
     {
         [Serializable]
-        public enum OtherEffectType
-        {
-            SpecialEffect,
-            Grow,
-            LearnAbility,
-            CommonEvent
-        }
-
         public enum SpecialEffect
         {
-            Salut,
+            Fuire,
             Bonjour
         }
 
         public enum CommonEvent
         {
-            Hi,
-            Hello
+            One,
+            Two
         }
 
         #region Public Members
 
-        public OtherEffectType m_otherEffectType;
         public SpecialEffect m_specialEffect;
+
         public ParametersEnum m_parametersEnum;
         public int m_growAmount;
+
         public SkillData m_skill;
+
         public CommonEvent m_commonEvent;
 
         #endregion
