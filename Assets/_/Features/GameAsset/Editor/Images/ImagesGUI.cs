c19374746b9c84bb0c07e260f51fd266b5743b68
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-05-26 13:47:11 +0200

    [Images] Fixed ImagesGUI script to be used in global project

--- Assets/_/Features/GameAsset/Editor/Images/ImagesGUI.cs
+++ Assets/_/Features/GameAsset/Editor/Images/ImagesGUI.cs
@@ -4,119 +4,90 @@ using UnityEngine;
 
 namespace GameAsset.Editor
 {
-    public class ImagesGUI : EditorWindow
+    public class ImagesGUI
     {
-        [MenuItem("Tools/Images")]
-        public static void ShowWindow()
-        {
-            var window = GetWindow<ImagesGUI>();
-            window.titleContent.text = "Images";
-        }
-
         #region Public Members
 
         public Images m_imagesData;
-        public ActorData m_actorData;
-
-        #endregion
-
-
-        #region Unity API
-
-        private void OnGUI()
-        {
-            GetGUI();
-        }
 
         #endregion
 
 
         #region Main Methods
 
-        public void GetGUI()
+        public void GetGUI(Images data)
         {
-            SetImageDataInstanceAsToBeDELETED();
-
+            SetImageDataInstance(data);
 
             if (m_imagesData == null) return;
 
-            GUIStyle boldFontStyle = new GUIStyle();
-            boldFontStyle.fontStyle = FontStyle.Bold;
-            boldFontStyle.normal.textColor = Color.white;
-            GUILayout.Label("Images", boldFontStyle);
-            
+            Display();
+        }
+
+        private void Display()
+        {
+            GUILayout.Label("Images", GetStyle());
+
             GUILayout.BeginHorizontal();
 
             FaceField();
-
             CharacterField();
-
             FighterField();
 
             GUILayout.EndHorizontal();
         }
 
-        public void SetImageDataInstance(Images instance)
+        #endregion
+
+
+        #region Utils
+
+        private void SetImageDataInstance(Images instance)
         {
             m_imagesData = instance;
         }
 
-        private void SetImageDataInstanceAsToBeDELETED()
+        private GUIStyle GetStyle()
         {
-            m_actorData = EditorGUILayout.ObjectField(m_actorData, typeof(ActorData), false) as ActorData;
-            if (m_actorData == null) return;
-            m_imagesData = m_actorData.m_images;
-            EditorGUILayout.Space();
+            GUIStyle boldFontStyle = new GUIStyle();
+            boldFontStyle.fontStyle = FontStyle.Bold;
+            boldFontStyle.normal.textColor = Color.white;
+
+            return boldFontStyle;
         }
 
-        public void FaceField()
+        private void FaceField()
         {
             GUILayout.BeginVertical();
 
             GUILayout.Label("Face:");
 
-            _faceSprite = m_imagesData.m_faceSprite; // Should be done once
-            _faceSprite = (Sprite)EditorGUILayout.ObjectField(_faceSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f));
-            m_imagesData.m_faceSprite = _faceSprite;
+            m_imagesData.m_faceSprite = EditorGUILayout.ObjectField(m_imagesData.m_faceSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f)) as Sprite;
 
             GUILayout.EndVertical();
         }
 
-        public void CharacterField()
+        private void CharacterField()
         {
             GUILayout.BeginVertical();
 
             GUILayout.Label("Character:");
 
-            _characterSprite = m_imagesData.m_characterSprite; // Should be done once
-            _characterSprite = (Sprite)EditorGUILayout.ObjectField(_characterSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f));
-            m_imagesData.m_characterSprite = _characterSprite;
+            m_imagesData.m_characterSprite = EditorGUILayout.ObjectField(m_imagesData.m_characterSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f)) as Sprite;
 
             GUILayout.EndVertical();
         }
 
-        public void FighterField()
+        private void FighterField()
         {
             GUILayout.BeginVertical();
 
             GUILayout.Label("[SV] Fighter :");
 
-            _fighterSprite = m_imagesData.m_fighterSprite; // Should be done once
-            _fighterSprite = (Sprite)EditorGUILayout.ObjectField(_fighterSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f));
-            m_imagesData.m_fighterSprite = _fighterSprite;
+            m_imagesData.m_fighterSprite = EditorGUILayout.ObjectField(m_imagesData.m_fighterSprite, typeof(Sprite), false, GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f)) as Sprite;
 
             GUILayout.EndVertical();
         }
-     
-
-        #endregion
-
-
-        #region Private and Protected Members
-
-        private Sprite _faceSprite;
-        private Sprite _characterSprite;
-        private Sprite _fighterSprite;
 
         #endregion
     }
