e8dd6850dc9f0a1152797d40ae141c46973c2552
Gabriel Nunez <gabriel2001@hotmail.be>
2023-05-26 11:50:13 +0200

    [GeneralParameters] Added GeneralParametersActor script with testing

--- Assets/_/Features/GameAsset/Editor/GeneralParameters/GeneralParametersActorGUI.cs
+++ Assets/_/Features/GameAsset/Editor/GeneralParameters/GeneralParametersActorGUI.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using GameAsset.Runtime;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace GameAsset.Editor
@@ -16,45 +14,7 @@ namespace GameAsset.Editor
 
         private void OnGUI()
         {
-            string[] guids = AssetDatabase.FindAssets("t:MyScriptableObject");
-
-            GUILayout.Label("General Parameters");
-
-            // Line 1
-            GUILayout.BeginHorizontal();
-            GUILayout.BeginVertical();
-            GUILayout.Label("Name:");
-            _nameTextField = GUILayout.TextField(_nameTextField);
-            GUILayout.EndVertical();
-
-            GUILayout.Space(5);
-
-            GUILayout.BeginVertical();
-            GUILayout.Label("Nickname:");
-            _nickNameTextField = GUILayout.TextField(_nickNameTextField);
-            GUILayout.EndVertical();
-            GUILayout.EndHorizontal();
-
-            // Line 2
-            GUILayout.BeginHorizontal();
-            GUILayout.BeginVertical();
-            GUILayout.Label("Class:");
-            _nameTextField = GUILayout.TextField(_nameTextField);
-            GUILayout.EndVertical();
-
-            GUILayout.Space(5);
-
-            GUILayout.BeginHorizontal();
-            GUILayout.BeginVertical();
-            GUILayout.Label("Initial Level:");
-            _nickNameTextField = GUILayout.TextField(_nickNameTextField);
-            GUILayout.EndVertical();
-            GUILayout.BeginVertical();
-            GUILayout.Label("Max Level:");
-            _nickNameTextField = GUILayout.TextField(_nickNameTextField);
-            GUILayout.EndVertical();
-            GUILayout.EndHorizontal();
-            GUILayout.EndHorizontal();
+            GetGUI();
         }
 
         #endregion
@@ -68,6 +28,116 @@ namespace GameAsset.Editor
             window.titleContent = new GUIContent("Tool");
         }
 
+        public void GetGUI()
+        {
+            _actor = (ActorData)EditorGUILayout.ObjectField(_actor, typeof(ActorData), false);
+
+            GUIStyle boldFontStyle = new()
+            {
+                fontStyle = FontStyle.Bold,
+            };
+            boldFontStyle.normal.textColor = Color.white;
+            GUILayout.Label("General Parameters", boldFontStyle);
+
+            GUILayout.BeginHorizontal();
+            NameField();
+            if (Event.current.type == EventType.Repaint)
+            {
+                _nameFieldWidth = GUILayoutUtility.GetLastRect().width;
+            }
+            GUILayout.Space(15);
+            NicknameField();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            ClassField();
+            GUILayout.Space(15);
+            InitialLevelField();
+            MaxLevelField();
+            GUILayout.EndHorizontal();
+            ProfileField();
+        }
+
+        private void NameField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Name:");
+            _nameTextField = _actor.m_generalParameters._name;
+            _nameTextField = EditorGUILayout.TextField(_nameTextField);
+            if (_actor.m_generalParameters._name != _nameTextField)
+            {
+                _actor.m_generalParameters._name = _nameTextField;
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void NicknameField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Nickname:");
+            _nickNameTextField = _actor.m_generalParameters.m_username;
+            _nickNameTextField = EditorGUILayout.TextField(_nickNameTextField);
+            if (_actor.m_generalParameters.m_username != _nickNameTextField)
+            {
+                _actor.m_generalParameters.m_username = _nickNameTextField;
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void ClassField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Class:");
+            _classObjectField = _actor.m_generalParameters.m_class;
+            _classObjectField = (ClassData)EditorGUILayout.ObjectField(_classObjectField, typeof(ClassData), false, GUILayout.Width(_nameFieldWidth));
+            if (_actor.m_generalParameters.m_class != _classObjectField)
+            {
+                _actor.m_generalParameters.m_class = _classObjectField;
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void InitialLevelField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Initial Level:");
+            _initialLevelIntField = _actor.m_generalParameters.m_initialLevel;
+            _initialLevelIntField = EditorGUILayout.IntField(_initialLevelIntField);
+            if (_actor.m_generalParameters.m_initialLevel != _initialLevelIntField)
+            {
+                _actor.m_generalParameters.m_initialLevel = _initialLevelIntField;
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void MaxLevelField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Max Level:");
+            _maxLevelIntField = _actor.m_generalParameters.m_maxLevel;
+            _maxLevelIntField = EditorGUILayout.IntField(_maxLevelIntField);
+            if (_actor.m_generalParameters.m_maxLevel != _maxLevelIntField)
+            {
+                _actor.m_generalParameters.m_maxLevel = _maxLevelIntField;
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void ProfileField()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Profile:");
+            _profileFieldVectorPosition = GUILayout.BeginScrollView(_profileFieldVectorPosition, GUILayout.Height(80));
+            _profileField = _actor.m_generalParameters.m_profile;
+            _profileField = GUILayout.TextArea(_profileField, GUILayout.ExpandHeight(true));
+            if (_actor.m_generalParameters.m_profile != _profileField)
+            {
+                _actor.m_generalParameters.m_profile = _profileField;
+            }
+            GUILayout.EndScrollView();
+            GUILayout.EndVertical();
+        }
+
         #endregion
 
         #region Utils
@@ -77,7 +147,17 @@ namespace GameAsset.Editor
         #region Private and Protected Members
 
         private string _nameTextField;
+        private float _nameFieldWidth;
         private string _nickNameTextField;
+
+        private ClassData _classObjectField;
+        private int _initialLevelIntField;
+        private int _maxLevelIntField;
+
+        private string _profileField;
+        private Vector2 _profileFieldVectorPosition;
+
+        private ActorData _actor;
 
         #endregion
     }
